78a0ebe26e87be3b3d0e9e8b2290405e6dd8bf23
Wanderson Henrique <slipkorn.jk@gmail.com>
2021-01-14 22:11:51 -0300

    Add files via upload

---
+++ case-dp6/js/tagueamento.js
@@ -0,0 +1,29 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+
+// Google Analytics 
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-12345-6', 'auto');
+
+// Script utilizado para realização de debug em ambiente local
+// https://www.google-analytics.com/analytics_debug.js
+// Configura modo debug 
+//if (location.hostname == 'localhost') {
+//    ga('set', 'sendHitTask', null);
+//}
+//window.ga_debug = {trace: true};
+// Fim modo debug
+
+ga('send', 'pageview');
+
+// End Google Analytics 
